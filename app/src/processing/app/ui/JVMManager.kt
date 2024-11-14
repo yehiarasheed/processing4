27313b24f388743b7acc5e57f15045316f8e61c4
Stef Tervelde <stef@steftervelde.nl>
2024-11-14 19:08:17 +0100

    Proper styling

--- app/src/processing/app/ui/JVMManager.kt
+++ app/src/processing/app/ui/JVMManager.kt
@@ -1,16 +1,28 @@
 package processing.app.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposePanel
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.dp
 import processing.app.Language
 import processing.app.Platform
@@ -18,11 +30,10 @@ import java.awt.BorderLayout
 import java.awt.Desktop
 import java.io.File
 import java.net.URI
-import javax.swing.Box
 import javax.swing.SwingUtilities
 
 
-fun addJDKManger(parent: Box){
+fun addJDKManger(parent: javax.swing.Box){
     SwingUtilities.invokeLater {
         val composePanel = ComposePanel().apply {
             setContent {
@@ -38,13 +49,25 @@ fun addJDKManger(parent: Box){
 
 // If the user does not have a valid JDK installed on their system, this function will display a message and a button to download the JDK
 // Temporary workaround until we ship Processing with a JDK again
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun JDKManager(){
     val home = Platform.getJavaHome()
     val valid = File(home, "bin/java").exists()
     if(valid) return
 
-    val color = Theme.getColor("editor.gradient.bottom")
+    val color = Theme.getColor("status.notice.bgcolor")
+    val colorText = Theme.getColor("status.notice.fgcolor")
+    val buttonColor =   Theme.getColor("toolbar.button.enabled.field")
+    val buttonTextColor = Theme.getColor("toolbar.button.enabled.glyph")
+
+    val fontFamily = FontFamily(
+        Font(
+            resource = "font/ProcessingSansPro-Regular.ttf",
+            weight = FontWeight.W400,
+            style = FontStyle.Normal
+        )
+    )
 
     val os = System.getProperty("os.name")
     val arch = System.getProperty("os.arch")
@@ -54,18 +77,42 @@ fun JDKManager(){
         else -> "linux"
     }
 
-    Row(modifier = Modifier
-        .background(color = Color(color.rgb))
-        .padding(8.dp)
-        .fillMaxWidth()
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(color.rgb)),
+        contentAlignment = Alignment.Center
     ) {
-        Text(Language.text("editor.status.missing.jvm"))
-        Spacer(modifier = Modifier.weight(1f))
-        Button(onClick = { openWebpage(URI("https://api.adoptium.net/v3/installer/latest/17/ga/${platform}/${arch}/jre/hotspot/normal/eclipse?project=jdk")) }) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp, 8.dp)
+                .fillMaxWidth()
+        ) {
             Text(
-                text = "Open a link",
-                color = Color.Black
+                "JDK not found. Please download the JDK to run Processing.",
+                fontFamily = fontFamily,
+                modifier = Modifier.align(Alignment.CenterVertically),
+                color = Color(colorText.rgb)
             )
+            Spacer(modifier = Modifier.weight(1f))
+            Row(
+
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .clickable{
+                        openWebpage(URI("https://api.adoptium.net/v3/installer/latest/17/ga/${platform}/${arch}/jre/hotspot/normal/eclipse?project=jdk"))
+                    }
+                    .background(color = Color(buttonColor.rgb))
+                    .padding(16.dp, 8.dp)
+
+                ,
+            ) {
+                Text(
+                    text = "Download JDK",
+                    color = Color(buttonTextColor.rgb),
+                    fontFamily = fontFamily
+                )
+            }
         }
     }
 }
