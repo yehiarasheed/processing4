989c70d741cc8d817c27f0f314d722df2bf62fbe
Stef Tervelde <stef@steftervelde.nl>
2024-12-17 02:46:59 +0100

    versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 group = "org.processing"
-version = "4.3.2"
+
+// Grab the latest version from the GitHub API
+val apiUrl = "https://api.github.com/repos/processing/processing4/releases"
+val response = java.net.URL(apiUrl).readText()
+val tagName = response.substringAfter("\"tag_name\":\"").substringBefore("\"")
+val currentVersion = tagName.split("-").last()
+version = currentVersion.split(".").let { parts ->
+    "${parts[0]}.${parts[1]}.${parts[2].toInt() + 1}"
+}
 
 plugins {
     kotlin("jvm") version libs.versions.kotlin apply false
